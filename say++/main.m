659f648a917ddead062520b8c133c5af5b75b1a5
Mark Smith <codeaddict@marks-macbook-pro.local>
2018-12-07 20:36:47 +0000

    Initial Commit

---
+++ say++/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  say++
+//
+//  Created by Mark Smith on 07/12/2018.
+//  Copyright © 2018 Mark Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
